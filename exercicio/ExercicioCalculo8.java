55a7069034b9e3081f0f275e27aa04faef6e77c2
rebeca2020 <61558667+rebeca2020@users.noreply.github.com>
2021-12-19 19:49:52 -0300

    ExercicioMod8

---
+++ exercicio/ExercicioCalculo8.java
@@ -0,0 +1,31 @@
+package br.com.rebeca.exercicio;
+
+public class ExercicioCalculo8 {
+
+	public static void main(String[] args) {
+
+		media();
+
+	}
+
+
+	private static void media() {
+	
+		int n1 = 10;
+		int n2 = 5;
+		int n3 = 7;
+		int n4 = 8;
+		
+		double soma = n1 + n2 + n3 + n4;
+		System.out.println(soma);
+		
+		
+		double mediaTotal = soma / 4;
+		System.out.println(mediaTotal);
+		
+		
+
+		
+	}
+
+}
